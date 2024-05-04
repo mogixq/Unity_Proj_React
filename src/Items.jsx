b8a5c6c0687a4e00a7b473feca1b8f38be640fe1
Almog <132549698+mogixq@users.noreply.github.com>
2024-05-04 21:13:02 +0300

    items with colors

--- src/Items.jsx
+++ src/Items.jsx
@@ -3,14 +3,17 @@ import ItemCard from './ItemCard'
 
 function Items() {
 
-    const HP_Desc = "This is a Health Potion, it heals the player's health points after he has taken damage from monsters or the environment"
+    const HP_Desc = "This is a Health Potion, it heals the player's health points after he has taken damage from monsters or the environment."
     const HP_Img = `.\\images\\HP.png`
 
-    const MP_Desc = "This is a Mana Potion, it refills the player's mana points after he has used mana to activate abilities"
+    const MP_Desc = "This is a Mana Potion, it refills the player's mana points after he has used mana to activate abilities."
     const MP_Img = `.\\images\\MP.png`
 
-    const EG_Desc = "This is an energy Potion, it gives a player temporary monement speed buff, allowing the player to evade monsters and traverse the map quicker. almost like an energy drink"
+    const EG_Desc = "This is an energy Potion, it gives a player temporary monement speed buff, allowing the player to evade monsters and traverse the map quicker. almost like an energy drink."
     const EG_Img = `.\\images\\EG.png`
+
+    const MaxHP_Desc = "This is a MaxHealth Heart, it increases the maximum possible health points the player can have, making the player able to survive more hits. also enables the player to survive otherwise lethal damage. quite a rare find."
+    const MaxHP_Img = `.\\images\\MaxHP.png`
 
   return (
     <>
@@ -18,7 +21,7 @@ function Items() {
     <ItemCard name ='HP Potion' info ={HP_Desc} ImgSrc={HP_Img} rarity ='Common' RarColor ='green'/>
     <ItemCard name ='MP Potion' info ={MP_Desc} ImgSrc={MP_Img} rarity ='Common' RarColor='green'/>
     <ItemCard name ='Energy Potion' info ={EG_Desc} ImgSrc={EG_Img} rarity ='Uncommon' RarColor='purple'/>
-    <ItemCard name ='HP Potion' info ={HP_Desc} ImgSrc={HP_Img} rarity ='Rare' RarColor='Orange'/>
+    <ItemCard name ='MaxHealth Heart' info ={MaxHP_Desc} ImgSrc={MaxHP_Img} rarity ='Rare' RarColor='Orange'/>
     <button onClick={() => window.scrollTo(0, 0)}>Scroll to top</button>
     </>
   )
