cfa0cc76e3986ca34f451e4bfb5ed4b1865059e8
Almog <132549698+mogixq@users.noreply.github.com>
2024-05-05 11:36:02 +0300

    monsters

--- src/Monsters.jsx
+++ src/Monsters.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import MonsCard from "./MonsCard";
 
 function Monsters() {
+  const Gob_Desc =
+    "This is a Health Potion, it heals the player's health points after he has taken damage from monsters or the environment.";
+  const Gob_Img = `.\\images\\HP.png`;
+
   return (
-    <div>Monsters</div>
-  )
+    <>
+      <h2>Monsters</h2>
+      <p>The foes that can be encountered in the game world</p>
+      <MonsCard
+        name="Goblin"
+        info={Gob_Desc}
+        ImgSrc={Gob_Img}
+        AttaRang="near"
+      />
+    </>
+  );
 }
 
-export default Monsters
+export default Monsters;
