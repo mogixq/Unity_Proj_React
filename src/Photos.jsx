e7c1fff606cf038994f6caad43af67b659e1b722
Almog <132549698+mogixq@users.noreply.github.com>
2024-05-06 13:12:31 +0300

    photo gallery

--- src/Photos.jsx
+++ src/Photos.jsx
@@ -4,9 +4,9 @@ function Photos(props) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const photos = [
-    '.\\images\\EG.png',
-    '.\\images\\HP.png',
-    '.\\images\\MP.png',
+    ".\\images\\3.png",
+    ".\\images\\2.png",
+    ".\\images\\1.png",
   ];
 
   const handleNext = () => {
@@ -21,10 +21,26 @@ function Photos(props) {
 
   return (
     <div>
+      <button onClick={() => props.changeDarkMode()}>Dark mode on/off</button> <br />
+      <h2>Photo Gallery</h2>
+      <p>View photos from the game and it's development</p> <br />
       <button onClick={handlePrev}>Previous</button>
       <button onClick={handleNext}>Next</button> <br />
-      <img src={photos[currentIndex]} alt={`Photo ${currentIndex}`} />
-      
+      <div
+        style={{
+          border: "2px solid purple",
+          borderRadius: 10,
+          maxWidth: 400,
+          maxHeight: 250,
+          padding:5
+        }}
+      >
+        <img
+          src={photos[currentIndex]}
+          alt={`Photo ${currentIndex}`}
+          style={{ borderRadius: 10,maxWidth:350,maxHeight:200 }}
+        />
+      </div>
     </div>
   );
 }
